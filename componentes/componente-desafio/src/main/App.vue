37acc0eaeee66b5d1c3382139208ccc3aacd9ee9
Jéssica Bohn <jessicabohn74@gmail.com>
2020-10-21 17:02:05 -0300

    ending componentes-desafio

---
+++ componentes/componente-desafio/src/main/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <appMenu />
+    <app-toolbar />
+    <v-content>
+      <Carousel />
+    </v-content>
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import Carousel from "@/components/widgets/Carousel.vue";
+import Footer from "../components/template/Footer.vue";
+import Menu from "../components/template/Menu.vue";
+import Toolbar from "../components/template/Toolbar.vue";
+
+export default {
+  components: {
+    Carousel,
+    Footer: Footer,
+    appMenu: Menu,
+    appToolbar: Toolbar,
+  },
+};
+</script>
