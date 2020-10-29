641c640e6cfb873739ad7e65284caa709ae87bb1
Jéssica Bohn <jessicabohn74@gmail.com>
2020-10-28 23:20:24 -0300

    update tarefas

--- projeto-02-tarefas/README.md
+++ projeto-02-tarefas/README.md
@@ -9,3 +9,7 @@
 - **[App.vue](https://github.com/TheJessicaBohn/VueJS/tree/master/projeto-02-tarefas/src/App.vue)** contem o style da pagina, a lem de ser o pai dos outros componentes
 - **[TaskGrid.vue](https://github.com/TheJessicaBohn/VueJS/tree/master/projeto-02-tarefas/src/components/TaskGrid.vue)** componente que contem a lista de tarefas, além de ser o componente pai de Task
 - **[Task.vue](https://github.com/TheJessicaBohn/VueJS/tree/master/projeto-02-tarefas/src/components/Task.vue)**
+
+## Dicionário
+- `cursor: pointer;` faz o cursor ser uma mãozinha
+- `user-select: none;` não deixa o usuário selecionar o texto
--- projeto-02-tarefas/src/components/Task.vue
+++ projeto-02-tarefas/src/components/Task.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="task"
-        :class="stateClass">
+  <div class="task" :class="stateClass">
     <p>{{ task.name }}</p>
   </div>
 </template>
@@ -8,18 +7,45 @@
 <script>
 export default {
   props: {
-    tasks: { type: Object, required: true },
+    task: { type: Object, required: true },
   },
   computed: {
     stateClass() {
       return {
         pending: this.task.pending,
-        done: !this.task.pending,
-      };
-    },
-  },
-};
+        done: !this.task.pending
+      }  
+    }
+  }
+}
 </script>
 
 <style>
+.task{
+    box-sizing: border-box;
+    width: 350px;
+    height: 150px;
+    padding: 10px;
+    border-radius: 8px;
+    font-size: 2rem;
+    font-weight: 300;
+    cursor: pointer;
+    user-select: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.pending{
+    border-left: 12px solid #b73229;
+    background-color: #f44336;
+}
+
+.done{
+    color: #ddd ;
+    border-left: 12px solid #0a8f08;
+    background-color: #4caf50;
+    text-decoration: line-through ;
+
+}
 </style>
