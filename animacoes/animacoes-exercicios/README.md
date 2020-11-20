aa0562d7bd5890cce14aaa377042d906e8b71b62
Jessica Bohn <jessicabohn74@gmail.com>
2020-11-20 13:30:55 -0300

    update animacoes-exercicios

--- animacoes/animacoes-exercicios/README.md
+++ animacoes/animacoes-exercicios/README.md
@@ -1,5 +1,12 @@
 # animacoes-exercicios
 
+## Códigos
+- **[App.vue](https://github.com/TheJessicaBohn/VueJS/tree/master/animacoes/animacoes-exercicios/src/App.vue)** trata os efeitos das mensagens, e o efeito da div
+- **[AlertaAdvertencia.vue](https://github.com/TheJessicaBohn/VueJS/tree/master/animacoes/animacoes-exercicios/src/AlertaAdvertencia.vue)**
+- **[AlertaInfo.vue](https://github.com/TheJessicaBohn/VueJS/tree/master/animacoes/animacoes-exercicios/src/AlertaInfo.vue)**
+##Transições
+`<transitions>`
+**area vazia -> animação = div -> animação = area vazia**
 - animações possui a utilização do bootstrap vue que pode ser encotrada em VueJS\animacoes\animacoes-exercicios\src\plugins
 - É possivel fazer as transições por css e java script`<transition>`.Exemplo:
     - Quando se inicia a animação   `*-enter `\
@@ -7,19 +14,37 @@
 	- Destino da animação`*-enter-to `\
 	
 	- Estado inicial, saída`*-leave `\
-	- Durante o processo de transição, saída`*-leave-active `\:arrow_left: **Elemento**:
+	- Durante o processo de transição, saída`*-leave-active `\**Elemento**:arrow_right:
 	- Estado final, saída`*-leave-to `\
 - A pricipio os elementos ficam escondidos e no final também
 - `@keyframes slide-in/slide-out` muda a posição nos eixos XY, e alem disso existe as classes enter e leave para o slide
 - Quando utilizadas duas ou mais animações é importante tomar cuidado com a diferença de tempo de cada pois uma pode interferir no desempenho da outra
 - Então o type vai evitar que esse tipo de conflito ocorra `type="animation"`, pois ele define qual transição conduz o elemento
+- Em index.html é usada a cdn do animate css para a que possam ser modificas as transitions de forma personalizada
 
-em index.htm é usada a cdn do animate css para a que possam ser modificas as transitions de forma personalizada
-wrap
+##Transições: Gatilhos JS
+- Transiçõesapenas com javascript
+	- `before-enter` antes entrar
+	- `enter` entrar
+	- `after-enter` depois de entrar
+	- `enter-cancelled` cancelada
 
-area vazia -> animação = div -> animação = area vazia
+	:arrow_right:**Elemento**:arrow_right:
+
+	- `before-leave` antes sair
+	- `leave` sair
+	- `after-leave` depois de sair
+	- `leave-cancelled` cancelada
+
+`<transitions-group>`
+- A tag do `<transitions>` não gera nenhuma tag no html final, diferente da tag`<transitions-group>` que gera uma tag `<span>`
+
+
+
+
 
 ## Dicionario
 -`appear` faz com que no moneto em que o refresh é dado a transição seja acatada
 - `mode="out-in"` primeiro o elemento sai pra depois o outro entrar, e pode ser usado ao contrário
+-`:css="false"` desativa qualquer influncia do css 
 
--- animacoes/animacoes-exercicios/src/App.vue
+++ animacoes/animacoes-exercicios/src/App.vue
@@ -1,96 +1,205 @@
 <template>
-	<div id="app" class="container-fluid">
-		<h1>Animações</h1>
-		<hr>
-		<b-button variant="primary" class="mb-4"
-		@click="exibir=!exibir">Mostrar Mensagem</b-button>
-		
-		<transition name="fade">
-			<b-alert variant="warning" show v-if="exibir">{{msg}}</b-alert>
-		</transition>
-		<hr>		
-		<transition name="slide" type="animation" appear>
-			<b-alert variant="danger" show v-show="exibir">{{msg}}</b-alert>
-		</transition>
-		<hr>
-		<transition 
-			enter-active-class="animated bounce"
-			leave-active-class="animated shake"
-			>
-			<b-alert variant="info" show v-show="exibir">{{msg}}</b-alert>
-		</transition>
-		<hr>
-		<b-select v-model="tipoAnimacao">
-			<option value="fade">Fade</option>
-			<option value="slide">Slide</option>
-		</b-select>
-		<transition :name="tipoAnimacao" class="mb-4" mode="out-in"> 
-			<b-alert variant="dark" show v-if="exibir" key="dark">{{msg}}</b-alert>
-			<b-alert variant="success" show v-else key="success">{{msg}}</b-alert>
-		</transition>
+  <div id="app" class="container-fluid">
+    <h1>Animações</h1>
+    <hr />
+    <b-button variant="primary" class="mb-4" @click="exibir = !exibir"
+      >Mostrar Mensagem</b-button
+    >
 
-	
+    <transition name="fade">
+      <b-alert variant="warning" show v-if="exibir">{{ msg }}</b-alert>
+    </transition>
+    <hr />
+    <transition name="slide" type="animation" appear>
+      <b-alert variant="danger" show v-show="exibir">{{ msg }}</b-alert>
+    </transition>
+    <hr />
+    <transition
+      enter-active-class="animated bounce"
+      leave-active-class="animated shake"
+    >
+      <b-alert variant="info" show v-show="exibir">{{ msg }}</b-alert>
+    </transition>
+    <hr />
+    <b-select v-model="tipoAnimacao">
+      <option value="fade">Fade</option>
+      <option value="slide">Slide</option>
+    </b-select>
+    <transition :name="tipoAnimacao" class="mb-4" mode="out-in">
+      <b-alert variant="dark" show v-if="exibir" key="dark">{{ msg }}</b-alert>
+      <b-alert variant="success" show v-else key="success">{{ msg }}</b-alert>
+    </transition>
+    <hr />
+    <button @click="exibir2 = !exibir2">Alternar</button>
+    <transition
+      :css="false"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @enter-cancelled="enterCancelled"
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+      @leave-cancelled="leaveCancelled"
+    >
+      <div v-if="exibir2" class="caixa"></div>
+    </transition>
+    <hr />
+    <div class="mb-4">
+      <b-button
+        variant="primary"
+        class="mr-2"
+        @click="componenteSelecionado = 'AlertaInfo'"
+        >Info</b-button
+      >
+      <b-button
+        variant="danger"
+        @click="componenteSelecionado = 'AlertaAdvertencia'"
+        >Advertência</b-button
+      >
+    </div>
+    <transition name="fade" mode="out-in">
+      <component :is="componenteSelecionado"> </component>
+    </transition>
+	<hr>
+	<b-button @click="adicionarAluno" class="mb-4">Adicionar Aluno </b-button>
+	<b-list-group v-for="(aluno, i) in alunos" :key="aluno">
+		<b-list-group-item @click="removerAluno(i)">{{aluno}}</b-list-group-item>
+	</b-list-group>
 
-	</div>
+
+
+  </div>
 </template>
 
 <script>
+import AlertaAdvertencia from "./AlertaAdvertencia";
+import AlertaInfo from "./AlertaInfo";
 
 export default {
-	data(){
-		return{
-			msg: 'Uma mensagem de informação para o usuario!',
-			exibir: true,
-			tipoAnimacaoo: 'fade'
-		}
-	}
-
-}
+  components: { AlertaAdvertencia, AlertaInfo },
+  data() {
+    return {
+      alunos: ['Andre', 'Jéssica', 'Fulano', 'Ciclano'],
+      msg: "Uma mensagem de informação para o usuario!",
+      exibir: false,
+      exibir2: true,
+      tipoAnimacao: "fade",
+      larguraBase: 0,
+      componenteSelecionado: "AlertaInfo",
+    };
+  },
+  methods: {
+	adicionarAluno(){
+		const s = Math.random().toString(36).substring(2)
+		this.alunos.push(s)
+	},
+	removerAluno(indice){
+		this.alunos.splice(indice, 1)
+	},
+    animar(el, done, negativo) {
+      let rodada = 1;
+      const temporizador = setInterval(() => {
+        const novaLargura =
+          this.larguraBase + (negativo ? -rodada * 10 : rodada * 10);
+        el.style.width = `${novaLargura}px`;
+        rodada++;
+        if (rodada > 30) {
+          clearInterval(temporizador);
+          done();
+        }
+      });
+    },
+    beforeEnter(el) {
+      this.larguraBase = 0;
+      el.style.width = `${this.larguraBase}px`;
+    },
+    enter(el, done) {
+      this.animar(el, done, false);
+    },
+    // afterEnter(el) {
+    //   console.log("afterEnter");
+    // },
+    // enterCancelled() {
+    //   console.log("enterCancelled");
+    // },
+    beforeLeave(el) {
+      this.larguraBase = 300;
+      el.style.width = `${this.larguraBase}px`;
+    },
+    leave(el, done) {
+      this.animar(el, done, true);
+    },
+    //afterLeave(el) {
+    // console.log("afterLeave");
+    //},
+    // leaveCancelled() {
+    //   console.log("leaveCancelled");
+    // },
+  },
+};
 </script>
 
 <style>
 #app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-	font-size: 1.5rem;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 1.5rem;
+}
+.caixa {
+  height: 100px;
+  width: 300px;
+  margin: 30px auto;
+  background-color: palevioletred;
 }
 
-.fade-enter, .fade-leave-to{
-	opacity: 0;
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 
-.fade-enter-active, .fade-leave-active{
-	transition: opacity 2s;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 2s;
 }
 
-.fade-enter-to, .fade-leave{
-	opacity: 1;
+.fade-enter-to,
+.fade-leave {
+  opacity: 1;
 }
 
 @keyframes slide-in {
-	from{ transform: translateY(40px);}
-	to{ transform: translateY(0px);}
+  from {
+    transform: translateY(40px);
+  }
+  to {
+    transform: translateY(0px);
+  }
 }
 
 @keyframes slide-out {
-	from{ transform: translateY(0px);}
-	to{ transform: translateY(40px);}
+  from {
+    transform: translateY(0px);
+  }
+  to {
+    transform: translateY(40px);
+  }
 }
 
 .slide-enter-active {
-	animation: slide-in 2s ease;
-	transition: opacity 2s;
+  animation: slide-in 2s ease;
+  transition: opacity 2s;
 }
 .slide-leave-active {
-	animation: slide-out 2s ease;
-	transition: opacity 6s;
+  animation: slide-out 2s ease;
+  transition: opacity 6s;
 }
-.slide-enter, .slide-leave-to{
-	opacity: 0;
+.slide-enter,
+.slide-leave-to {
+  opacity: 0;
 }
-
 </style>
