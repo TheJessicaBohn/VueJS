27e512df3a6044487c4b7e2d8637d1e489f79773
Jéssica Bohn <jessicabohn74@gmail.com>
2020-11-19 23:35:05 -0300

    update animacoes-exercicios

--- animacoes/animacoes-exercicios/README.md
+++ animacoes/animacoes-exercicios/README.md
@@ -1,11 +1,25 @@
 # animacoes-exercicios
 
-animações possui a utilização do bootstrap vue que pode ser encotrada em VueJS\animacoes\animacoes-exercicios\src\plugins
+- animações possui a utilização do bootstrap vue que pode ser encotrada em VueJS\animacoes\animacoes-exercicios\src\plugins
+- É possivel fazer as transições por css e java script`<transition>`.Exemplo:
+    - Quando se inicia a animação   `*-enter `\
+	- Durante o processo de transição `*-enter-active `\  :arrow_right:**Elemento**
+	- Destino da animação`*-enter-to `\
+	
+	- Estado inicial, saída`*-leave `\
+	- Durante o processo de transição, saída`*-leave-active `\:arrow_left: **Elemento**:
+	- Estado final, saída`*-leave-to `\
+- A pricipio os elementos ficam escondidos e no final também
+- `@keyframes slide-in/slide-out` muda a posição nos eixos XY, e alem disso existe as classes enter e leave para o slide
+- Quando utilizadas duas ou mais animações é importante tomar cuidado com a diferença de tempo de cada pois uma pode interferir no desempenho da outra
+- Então o type vai evitar que esse tipo de conflito ocorra `type="animation"`, pois ele define qual transição conduz o elemento
 
+em index.htm é usada a cdn do animate css para a que possam ser modificas as transitions de forma personalizada
 wrap
-<transition>
+
 area vazia -> animação = div -> animação = area vazia
 
-## Project setup
-``
+## Dicionario
+-`appear` faz com que no moneto em que o refresh é dado a transição seja acatada
+- `mode="out-in"` primeiro o elemento sai pra depois o outro entrar, e pode ser usado ao contrário
 
--- animacoes/animacoes-exercicios/src/App.vue
+++ animacoes/animacoes-exercicios/src/App.vue
@@ -1,12 +1,49 @@
 <template>
 	<div id="app" class="container-fluid">
 		<h1>Animações</h1>
+		<hr>
+		<b-button variant="primary" class="mb-4"
+		@click="exibir=!exibir">Mostrar Mensagem</b-button>
+		
+		<transition name="fade">
+			<b-alert variant="warning" show v-if="exibir">{{msg}}</b-alert>
+		</transition>
+		<hr>		
+		<transition name="slide" type="animation" appear>
+			<b-alert variant="danger" show v-show="exibir">{{msg}}</b-alert>
+		</transition>
+		<hr>
+		<transition 
+			enter-active-class="animated bounce"
+			leave-active-class="animated shake"
+			>
+			<b-alert variant="info" show v-show="exibir">{{msg}}</b-alert>
+		</transition>
+		<hr>
+		<b-select v-model="tipoAnimacao">
+			<option value="fade">Fade</option>
+			<option value="slide">Slide</option>
+		</b-select>
+		<transition :name="tipoAnimacao" class="mb-4" mode="out-in"> 
+			<b-alert variant="dark" show v-if="exibir" key="dark">{{msg}}</b-alert>
+			<b-alert variant="success" show v-else key="success">{{msg}}</b-alert>
+		</transition>
+
+	
+
 	</div>
 </template>
 
 <script>
 
 export default {
+	data(){
+		return{
+			msg: 'Uma mensagem de informação para o usuario!',
+			exibir: true,
+			tipoAnimacaoo: 'fade'
+		}
+	}
 
 }
 </script>
@@ -21,4 +58,39 @@ export default {
 	margin-top: 60px;
 	font-size: 1.5rem;
 }
+
+.fade-enter, .fade-leave-to{
+	opacity: 0;
+}
+
+.fade-enter-active, .fade-leave-active{
+	transition: opacity 2s;
+}
+
+.fade-enter-to, .fade-leave{
+	opacity: 1;
+}
+
+@keyframes slide-in {
+	from{ transform: translateY(40px);}
+	to{ transform: translateY(0px);}
+}
+
+@keyframes slide-out {
+	from{ transform: translateY(0px);}
+	to{ transform: translateY(40px);}
+}
+
+.slide-enter-active {
+	animation: slide-in 2s ease;
+	transition: opacity 2s;
+}
+.slide-leave-active {
+	animation: slide-out 2s ease;
+	transition: opacity 6s;
+}
+.slide-enter, .slide-leave-to{
+	opacity: 0;
+}
+
 </style>
