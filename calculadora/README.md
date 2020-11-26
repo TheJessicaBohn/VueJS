7e2aacd18495d1c199d48a4bf47dd7231eee6021
Jéssica Bohn <jessicabohn74@gmail.com>
2020-11-26 16:00:43 -0300

    create calculadora

---
+++ calculadora/README.md
@@ -0,0 +1,14 @@
+# Calculadora
+
+## Dicas
+- div#app para criar a div com id
+
+- Criar pasta calculadora
+- Instalar o vue-cli: `npm i -g @vue/cli`
+- `vue create calculator` escolher a versão padrão
+- `cd calculator`
+- `npm run serve`
+- apagar a pasta src, e cria-lá novamente
+- Criar e configurar `App.vue` e `main.js`
+- Criar pasta fonts
+- Baixar fonte em https://fonts.google.com/specimen/Roboto#standard-styles
---
+++ calculadora/calculator/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+      <h1>Olá CIDS !</h1>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@font-face {
+    font-family: "RobotoMono";
+    src: url("./fonts/Roboto-Thin.ttf");
+}
+
+*{
+    font-family: "RobotoMono", monospace;
+}
+
+body{
+    margin: 0;
+}
+
+#app{
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: white;
+    background: linear-gradient(to right, rgb(226, 190, 226),rgb(94, 3, 94));
+}
+
+
+</style>
